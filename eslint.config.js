3b34ce25675482866fd8297bdc72668f7d2a4a0a
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-01-10 03:54:56 +0100

    Probando

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,19 @@
 import prettierConfig from 'eslint-config-prettier'
 import prettierPlugin from 'eslint-plugin-prettier'
+
 export default {
-  files: ['**/*.js'],
-  ignores: ['node_modules/', 'dist/'],
-  plugins: { prettier: prettierPlugin },
-  rules: { ...prettierConfig.rules, 'prettier/prettier': 'error' },
+  overrides: [
+    {
+      files: ['**/*.js'], // Archivos a analizar
+      excludedFiles: ['node_modules/**', 'dist/**'], // Ignorar directorios
+      plugins: { prettier: prettierPlugin },
+      rules: {
+        ...prettierConfig.rules, // Configuración de Prettier
+        'prettier/prettier': 'error', // Lanza errores si no se sigue el formato
+        'no-unused-vars': 'error', // Lanza error si hay variables no usadas
+        quotes: ['error', 'single'], // Forzar comillas simples
+        semi: ['error', 'never'], // Forzar no usar punto y coma
+      },
+    },
+  ],
 }
--- task-management/config/db.js
+++ task-management/config/db.js
@@ -1,8 +1,8 @@
-// task-management/config/db.js// Ejemplo de código con un error intencional
-function probarHusky() {
-  console.log("Este mensaje se muestra antes del error)
+// task-management/config/db.js
+const connectDB = async () => {
+  console.log("Conectando a la base de datos...)  // Error: comilla no cerrada
+}
 
-  // Falta una comilla de cierre en el console.log. 
-  // Esto ocasionará un error de sintaxis y romperá la ejecución.
-};;
-)
+module.exports = connectDB
+
+
