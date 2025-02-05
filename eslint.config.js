049e75ccf0929aef495c00beb2b62934ba355aa7
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-02-06 00:18:13 +0100

    Mejora en las inconsistencias de eslint y prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,34 @@
-const prettierConfig = require('eslint-config-prettier')
-const prettierPlugin = require('eslint-plugin-prettier')
+import eslint from '@eslint/js'
+import prettierPlugin from 'eslint-plugin-prettier'
+import nodePlugin from 'eslint-plugin-node'
+import globals from 'globals'
 
-module.exports = [
+export default [
   {
-    files: ['**/*.js'], // Archivos que serán analizados
-    ignores: ['node_modules/**', 'dist/**'], // Directorios a ignorar
-    plugins: {
-      prettier: prettierPlugin, // Registrar el plugin de Prettier
+    files: ['**/*.js'], // Analiza todos los archivos JS
+    ignores: ['node_modules/**', 'dist/**'], // Ignora estas carpetas
+
+    languageOptions: {
+      ecmaVersion: 'latest', // Última versión de ECMAScript
+      sourceType: 'module', // Soporte para ESM
+      globals: {
+        ...globals.node, // Variables globales para Node.js
+      },
     },
+
+    plugins: {
+      node: nodePlugin, // Reglas específicas para Node.js
+      prettier: prettierPlugin, // Integración con Prettier
+    },
+
     rules: {
-      ...prettierConfig.rules, // Reglas de Prettier
-      'prettier/prettier': 'error', // Lanza errores si no se sigue el formato de Prettier
-      'no-unused-vars': 'error', // Lanza error si hay variables no usadas
+      'prettier/prettier': 'error', // Errores si no se sigue el formato de Prettier
+      'no-unused-vars': 'error', // Errores para variables no utilizadas
+      'no-console': 'warn', // Advertencias para el uso de console.log
+      'consistent-return': 'error', // Obliga a que las funciones devuelvan un valor de forma coherente
+      'node/no-unsupported-features/es-syntax': 'off', // Permitir ESM sin advertencias
       quotes: ['error', 'single'], // Forzar comillas simples
-      semi: ['error', 'never'], // Forzar no usar punto y coma
+      semi: ['error', 'never'], // No permitir punto y coma al final de las líneas
     },
   },
 ]
