7e1e00d1f89377601005fc78ccc92787cb69f0fa
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-07-20 17:18:08 +0200

    Trabajo en Cors, los allowOrigins están provocado errores de inicio de sesión o de registro

--- task-management/middlewares/security/corsMiddleware.js
+++ task-management/middlewares/security/corsMiddleware.js
@@ -1,22 +1,20 @@
-import cors from 'cors'
-import { isAllowedOrigin } from '../../../utils/origins/originUtils.js'
+const allowedOrigins = ['https://invexly.netlify.app']
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (isAllowedOrigin(origin)) {
-      console.log(`[CORS] Origin permitido: ${origin}`)
-      callback(null, true)
-    } else {
-      console.warn(`[CORS] Origin bloqueado: ${origin}`)
-      callback(new Error(`Origen no permitido por CORS: ${origin}`))
+const corsMiddleware = {
+  origin: (origin, callback) => {
+    if (!origin) {
+      // Silencia requests sin Origin (ej: bots, Render healthchecks)
+      return callback(null, true)
     }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-  exposedHeaders: ['Set-Cookie']
-}
 
-const corsMiddleware = cors(corsOptions)
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true)
+    }
+
+    // Producción: sin logs innecesarios
+    return callback(new Error('No autorizado por CORS'))
+  },
+  credentials: true
+}
 
 export default corsMiddleware
