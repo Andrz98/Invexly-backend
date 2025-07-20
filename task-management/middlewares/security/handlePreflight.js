23c2759e236df0fa75522920356e513baff15228
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-07-20 17:49:58 +0200

    Trabajando en preflight

--- task-management/middlewares/security/handlePreflight.js
+++ task-management/middlewares/security/handlePreflight.js
@@ -4,31 +4,10 @@
  * @param {Error} err Error arrojado por la aplicación.
  * @param {Request} req Objeto de solicitud HTTP.
  */
-const allowedOrigins = ['https://invexly.netlify.app']
-
-const handlePreflight = (req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    const origin = req.headers.origin
-
-    if (!origin || allowedOrigins.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin || '')
-      res.header('Access-Control-Allow-Credentials', 'true')
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PUT, DELETE, OPTIONS'
-      )
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization, Cookie'
-      )
-      return res.sendStatus(200)
-    }
-
-    return res.sendStatus(403)
+const handlePreflight = {
+  OPTIONS: (req, res) => {
+    res.sendStatus(204)
   }
-
-  // Para métodos distintos de OPTIONS, continuar el flujo
-  return next()
 }
 
 export default handlePreflight
