c45540f5503490e2f17cb091815d9c2bee39f42a
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-07-20 17:29:14 +0200

    Saneo handlePreflight del antiguo componente originUtils.

--- task-management/middlewares/security/handlePreflight.js
+++ task-management/middlewares/security/handlePreflight.js
@@ -1,18 +1,17 @@
-import { isAllowedOrigin } from '../../../utils/origins/originUtils.js'
-
 /**
  * Maneja los errores capturados en la aplicación Express.
  *
  * @param {Error} err Error arrojado por la aplicación.
  * @param {Request} req Objeto de solicitud HTTP.
  */
-const handlePreflight = (req, res) => {
+const allowedOrigins = ['https://invexly.netlify.app']
+
+const handlePreflight = (req, res, next) => {
   if (req.method === 'OPTIONS') {
     const origin = req.headers.origin
 
-    if (!origin || isAllowedOrigin(origin)) {
-      console.log(`[Preflight] Origin permitido: ${origin}`)
-      res.header('Access-Control-Allow-Origin', origin)
+    if (!origin || allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin || '')
       res.header('Access-Control-Allow-Credentials', 'true')
       res.header(
         'Access-Control-Allow-Methods',
@@ -22,15 +21,15 @@ const handlePreflight = (req, res) => {
         'Access-Control-Allow-Headers',
         'Content-Type, Authorization, Cookie'
       )
-
       return res.sendStatus(200)
-    } else {
-      console.warn(`[Preflight] Origin bloqueado: ${origin}`)
-      return res
-        .status(403)
-        .send('CORS policy: Origin no permitido en producción.')
     }
+
+    // En producción no se imprime nada, solo se rechaza.
+    return res.sendStatus(403)
   }
+
+  // Continuar si no es una preflight request
+  next()
 }
 
 export default handlePreflight
